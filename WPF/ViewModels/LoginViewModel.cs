964c19ab78277de1c9860eb34d267684c3dfe8a0
inspire-coding <inspire.coding.apps@gmail.com>
2022-07-14 11:05:04 +0200

    WPF: Finishing LoginView

--- WPF/ViewModels/LoginViewModel.cs
+++ WPF/ViewModels/LoginViewModel.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace WPF.ViewModels
 {
     public class LoginViewModel: ViewModelBase
     {
+        private string _username = "pazpeti";
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+                OnPropertyChanged(nameof(CanLogin));
+            }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+                OnPropertyChanged(nameof(CanLogin));
+            }
+        }
+
+        public bool CanLogin => !string.IsNullOrEmpty(Username) && !string.IsNullOrEmpty(Password);
+
+
+        public ICommand LoginCommand { get; }
+        public ICommand ViewRegisterCommand { get; }
     }
 }
