466d4a62432cc20238accc1771e4774af54d6f3c
inspire-coding <inspire.coding.apps@gmail.com>
2022-07-22 14:20:46 +0200

    WPF: .gitignore

--- WPF/obj/Debug/net6.0-windows/Views/HomeView.g.i.cs
+++ WPF/obj/Debug/net6.0-windows/Views/HomeView.g.i.cs
@@ -54,7 +54,7 @@ namespace WPF.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WPF;component/views/homeview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WPF;V1.0.0.0;component/views/homeview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\HomeView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
