e68af62c7ae247609e24d2294e3729b92f35f331
inspire-coding <inspire.coding.apps@gmail.com>
2022-08-01 13:27:35 +0200

    WPF: BooleanToGreenWhiteColorConverter

---
+++ WPF/Converters/BooleanToGreenWhiteColorConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WPF.Converters
+{
+    public class BooleanToGreenWhiteColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                bool _value = (bool)value;
+
+                if (_value)
+                {
+                    return Application.Current.Resources["BrushGreen"];
+                }
+                else
+                {
+                    return Application.Current.Resources["BrushWhite"];
+                }
+            }
+
+            return String.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
