daa36ad0a369ef31644d9ca4e0eee8961c008e57
inspire-coding <inspire.coding.apps@gmail.com>
2022-10-05 15:32:27 +0200

    WPF: update of shopping list view

--- WPF/ViewModels/ViewShoppingListViewModel.cs
+++ WPF/ViewModels/ViewShoppingListViewModel.cs
@@ -20,6 +20,28 @@ namespace WPF.ViewModels
         public ShoppingListDto ShoppingList { get; set; }
         public ItemListingViewModel ItemListingViewModel { get; }
 
+        private StringBuilder _members;
+        public string Members
+        { 
+            get
+            {
+                _members = new StringBuilder();
+                for (int i = 0; i < ShoppingList.Members.Count(); i++)
+                {
+                    if (i != ShoppingList.Members.Count()-1)
+                    {
+                        _members.Append($"{ShoppingList.Members.ElementAt(i).Username}, ");
+                    }
+                    else
+                    {
+                        _members.Append($"{ShoppingList.Members.ElementAt(i).Username}");
+                    }
+                }
+
+                return _members.ToString();
+            }
+        }
+
         public ViewShoppingListViewModel(ShoppingListStore shoppingListStore, INavigator navigator, IMemberService memberService, IItemService itemService)
         {
             _navigator = navigator;
